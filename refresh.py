ad96b4341136607f24769727d91ef3fc366d70a1
usmcsky <deathbyzombiez@gmail.com>
2025-01-06 20:55:20 -0600

    update

--- refresh.py
+++ refresh.py
@@ -10,3 +10,35 @@ else:
 
 if name == "USMCsky":
     print("time to retire!")
+
+
+#DEFINE FUNCTIONS
+def hello(name = "Chugs", age = 0):
+    print("Hello {} you are {} years old".format(name, age))
+
+hello("SKY", 55)
+
+sentence = hello()
+print(sentence)
+
+def tripleprint():
+    print("world " * 3)
+
+tripleprint()
+
+
+# LIST IS ARRAY BY ANOTHER NAME
+dognames = ["Fido", "Bones", "Sally", "Doji", 42, True]
+print(dognames)
+
+dognames.insert(0, "Jane")
+print(dognames[0])
+print(dognames)
+print(len(dognames))
+del(dognames[0])
+print(dognames)
+print(len(dognames))
+
+dognames[0] = "Sam"
+print(dognames)
+
