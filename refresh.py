2a2f014c892554f440eab1e0fa918da7c4ccd0ac
usmcsky <deathbyzombiez@gmail.com>
2025-01-06 19:13:33 -0600

    update

--- refresh.py
+++ refresh.py
@@ -1,1 +1,12 @@
-print("hello world")
+age = 55
+name = "USMCsky"
+
+print("Hello my name is {} and I am {} years old".format(name, age))
+
+if age > 16:
+    print("You are old enough to drive.")
+else:
+    print("not quite there yet.")
+
+if name == "USMCsky":
+    print("time to retire!")
